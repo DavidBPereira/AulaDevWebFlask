15d65f7995edd3234de4a0e099fba7656d6426cd
David Pereira <davidbelpereira@gmail.com>
2022-11-11 23:11:58 -0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 
 from flask import Flask
 from Login import Login_bp
-from FaleConosco import FaleConosco_bp
+
 from Impacta import Impacta_bp
 app = Flask(__name__)
 app.config.from_object('app.config.Configuracao')
 
-app.register_blueprint(FaleConosco_bp, url_prefix='/FaleConosco')
+
 app.register_blueprint(Impacta_bp,url_prefix= '/Impacta')
 app.register_blueprint(Login_bp,url_prefix= '/Login')
